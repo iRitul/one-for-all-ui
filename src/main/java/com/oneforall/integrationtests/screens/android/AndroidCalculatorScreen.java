d2a3d02354adf73ad4075f5e5dc831aa3361292e
Sameeksha Sahib <sameeksha.sahib@gmail.com>
2023-06-11 19:54:52 +0530

    added android tests

--- src/main/java/com/oneforall/integrationtests/screens/android/AndroidCalculatorScreen.java
+++ src/main/java/com/oneforall/integrationtests/screens/android/AndroidCalculatorScreen.java
@@ -1,45 +1,66 @@
 package com.oneforall.integrationtests.screens.android;
 
 import com.oneforall.integrationtests.screens.CalculatorScreen;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AndroidCalculatorScreen extends CalculatorScreen {
     public static final Logger LOG = Logger.getLogger(AndroidCalculatorScreen.class);
     private AndroidDriver driver;
+
+    private static String byNumberButtonIdText = "digit_'%s']";
+    private static By byPlusButtonId = MobileBy.id("op_add");
+    private static By byMinusButtonId = MobileBy.id("op_sub");
+    private static By byEqualToButtonId = MobileBy.id("eq");
+    private static By byFinalResultId = MobileBy.id("result_final");
+    private static By byResultPreviewId = MobileBy.id("result_preview");
+    private static By byDeleteButtonId = MobileBy.id("del");
+
+
     public AndroidCalculatorScreen(WebDriver driver) {
         this.driver = (AndroidDriver) driver;
     }
 
     @Override
     public CalculatorScreen clickNumber(int i) {
+        genericMethods.waitForClickabilityOf(getNumberButtonID(i)).click();
         return this;
     }
 
     @Override
     public CalculatorScreen clickOnPlusButton() {
+        genericMethods.waitForClickabilityOf(byPlusButtonId).click();
         return this;
     }
 
     @Override
     public CalculatorScreen clickOnDeleteButton() {
+        genericMethods.waitForClickabilityOf(byDeleteButtonId).click();
         return this;
     }
 
     @Override
     public CalculatorScreen clickOnEqualToButton() {
+        genericMethods.waitForClickabilityOf(byEqualToButtonId).click();
         return this;
     }
 
     @Override
     public int getResult() {
-        return 0;
+        return Integer.parseInt(genericMethods.waitForClickabilityOf(byFinalResultId).getText().trim());
     }
 
     @Override
     public CalculatorScreen clickOnMinusButton() {
+        genericMethods.waitForClickabilityOf(byMinusButtonId).click();
         return this;
+    }
+
+    private By getNumberButtonID(int number) {
+        return By.id(String.format(byNumberButtonIdText, number));
     }
 }
 
--- src/main/java/com/oneforall/integrationtests/screens/iOS/IosCalculatorScreen.java
+++ src/main/java/com/oneforall/integrationtests/screens/iOS/IosCalculatorScreen.java
@@ -10,9 +10,6 @@ import org.openqa.selenium.WebDriver;
 public class IosCalculatorScreen extends CalculatorScreen {
     public static final Logger LOG = Logger.getLogger(IosCalculatorScreen.class);
     private IOSDriver driver;
-    public IosCalculatorScreen(WebDriver driver) {
-        this.driver = (IOSDriver) driver;
-    }
 
     private static String byNumberButtonText = "//XCUIElementTypeButton[@name='%s']";
     private static By byPlusButtonXpath = MobileBy.xpath("//XCUIElementTypeButton[@name='+']");
@@ -20,14 +17,14 @@ public class IosCalculatorScreen extends CalculatorScreen {
     private static By byEqualsToButtonXpath = MobileBy.xpath("//XCUIElementTypeButton[@name='=']");
     private static By byResultAccessibilityId = new MobileBy.ByAccessibilityId("numberField");
 
+    public IosCalculatorScreen(WebDriver driver) {
+        this.driver = (IOSDriver) driver;
+    }
+
     @Override
     public CalculatorScreen clickNumber(int number) {
         genericMethods.waitForClickabilityOf(getNumberButtonXpath(number)).click();
         return this;
-    }
-
-    private By getNumberButtonXpath(int number) {
-        return By.xpath(String.format(byNumberButtonText, number));
     }
 
     @Override
@@ -56,5 +53,9 @@ public class IosCalculatorScreen extends CalculatorScreen {
     public CalculatorScreen clickOnMinusButton() {
         genericMethods.waitForClickabilityOf(byMinusButtonXpath).click();
         return this;
+    }
+
+    private By getNumberButtonXpath(int number) {
+        return By.xpath(String.format(byNumberButtonText, number));
     }
 }
